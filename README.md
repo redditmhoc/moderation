880b87cc723c2414d623b502d694cfa07a185821
Liesel Downes <liesel@lieseladl.xyz>
2022-06-15 19:40:16 +0930

    Remove old code

--- README.md
+++
@@ -1,8 +0,0 @@
-## MHoC Speakership
-
-Speakership web app for MHoC including moderation and vote counting
-
-### Contributors
-
-* Liesel (ellielia)
-* Viljo (vijlow)
--- public/js/utility.js
+++
@@ -1,6 +0,0 @@
-//Get ordinal helper
-var getGetOrdinal = function(n) {
-    var s=["th","st","nd","rd"],
-        v=n%100;
-    return n+(s[(v-20)%10]||s[v]||s[0]);
-}
